d33926c83eb9a9e86dbc72b3182e7e14752667f3
huangzijuan <huangzijuan@cmcm.com>
2020-03-09 17:15:07 +0800

    initial commit

---
+++ FacebookTabBar.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
+class FacebookTabBar extends React.Component {
+    icons = [];
+
+    constructor(props) {
+        super(props);
+        this.icons = [];
+    }
+
+    componentDidMount() {
+        //this._listener = this.props.scrollValue.addListener(this.setAnimationValue.bind(this));
+    }
+
+    setAnimationValue({value}) {
+        this.icons.forEach((icon, i) => {
+            const progress = (value - i >= 0 && value - i <= 1) ? value - i : 1;
+            // icon.setNativeProps({
+            //   style: {
+            //     color: this.iconColor(progress),
+            //   },
+            // });
+        });
+    }
+
+    //color between rgb(59,89,152) and rgb(204,204,204)
+    iconColor(progress) {
+        const red = 59 + (204 - 59) * progress;
+        const green = 89 + (204 - 89) * progress;
+        const blue = 152 + (204 - 152) * progress;
+        return `rgb(${red}, ${green}, ${blue})`;
+    }
+
+    render() {
+        return <View style={[styles.tabs, this.props.style]}>
+            {this.props.tabs.map((tab, i) => {
+                return <TouchableOpacity key={tab} onPress={() => this.props.goToPage(i)} style={styles.tab}>
+                    {/*<Icon*/}
+                    {/*  name={tab}*/}
+                    {/*  size={30}*/}
+                    {/*  color={this.props.activeTab === i ? 'rgb(59,89,152)' : 'rgb(204,204,204)'}*/}
+                    {/*  ref={(icon) => { this.icons[i] = icon; }}*/}
+                    {/*/>*/}
+
+                    <Text style={{
+                        color: this.props.activeTab === i ? 'rgb(59,89,152)' : 'rgb(204,204,204)',
+                        fontSize: this.props.activeTab === i ? 18 : 12,
+                    }}>
+                        {tab}
+                    </Text>
+                </TouchableOpacity>;
+            })}
+        </View>;
+    }
+}
+
+const styles = StyleSheet.create({
+    tab: {
+        // flex: 1,
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        // paddingBottom: 10,
+        alignSelf: 'center',
+        paddingLeft: 20,
+        paddingRight: 10,
+    },
+    tabs: {
+        height: 45,
+        flexDirection: 'row',
+        paddingTop: 5,
+        borderWidth: 1,
+        borderTopWidth: 0,
+        borderLeftWidth: 0,
+        borderRightWidth: 0,
+        borderBottomColor: 'rgba(0,0,0,0.15)',
+    },
+});
+
+export default FacebookTabBar;
---
+++ temp.md
@@ -0,0 +1,2 @@
+//"react-native-device-info": "^5.5.3",
+//"react-native-network-info": "^5.2.1",
